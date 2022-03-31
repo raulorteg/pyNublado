f5bf48a06b0cc1873a757abe22778e84a5e674bc
eor <eor@users.noreply.github.com>
2022-03-31 17:38:38 +0200

    t_{redshift} vs t_{stellar} filtering

--- src/sampling.py
+++ src/sampling.py
@@ -1,8 +1,11 @@
 import os
 import numpy as np
 from pyDOE import *
+import astropy.units as u
+from astropy.cosmology import Planck18
 
-from common.settings_parameters import PARAMETER_NUMBER, PARAMETER_LIMITS
+
+from common.settings_parameters import *
 from common.settings import PARAMETER_FILE
 from common.utils import *
 
@@ -34,8 +37,8 @@ def sampling_create_parameters(path, prefix, n_samples, filter=False, save_to_fi
     parameters = utils_rescale_parameters(limits=PARAMETER_LIMITS, parameters=lhs_normalised)
 
     if filter:
-        print('Filtering unphysical parameter combinations')
         parameters = sampling_filter_redshift_stellar_age(parameters)
+        n_samples = parameters.shape[0]
 
     if save_to_file:
         file_name = '{}_{}_N{}.npy'.format(prefix, PARAMETER_FILE, n_samples)
@@ -47,8 +50,38 @@ def sampling_create_parameters(path, prefix, n_samples, filter=False, save_to_fi
 
 
 def sampling_filter_redshift_stellar_age(parameters):
+    """
+    remove all parameter vectors for which the stellar age exceeds the age of the universe (given by the redshift)
 
-    # tba.
+    Args:
+        parameters: 2D array containing the parameters
+
+    Returns:
+        The filtered 2D parameter array
+    """
+
+    print('Filtering un-physical parameter combinations:')
+
+    n_samples_pre = parameters.shape[0]
+
+    z_column = PARAMETER_NUMBER_REDSHIFT - 1
+    t_column = PARAMETER_NUMBER_STELLAR_AGE - 1
+
+    redshifts = parameters[:, z_column]
+    stellar_ages = parameters[:, t_column]
+
+    # convert redshifts to ages
+    cosmology = Planck18
+    redshift_ages = cosmology.age(z=redshifts).to_value(u.Myr)
+
+    # create and apply mask (indexes True will be kept)
+    mask = redshift_ages > stellar_ages
+    parameters = parameters[mask]
+
+    # bookkeeping
+    n_samples_post = parameters.shape[0]
+    delta = n_samples_pre - n_samples_post
+    print('\t{} samples removed. {} remaining.'.format(delta, n_samples_post))
 
     return parameters
 
@@ -59,14 +92,12 @@ def sampling_filter_redshift_stellar_age(parameters):
 if __name__ == "__main__":
 
     target_directory = '../data/samples/'
-    target_prefix = 'test_sample'
-    N = 1000
+    target_prefix = 'test'
+    N = 100
 
     sampling_create_parameters(path=target_directory,
                                prefix=target_prefix,
                                n_samples=N,
-                               filter=False,
+                               filter=True,
                                save_to_file=True
                                )
-
-
