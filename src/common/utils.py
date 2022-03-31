ab090bea80d0f43636bcb6ad8cd85f0da65aa0df
eor <eor@users.noreply.github.com>
2022-03-31 14:46:56 +0200

    parameter sampling

--- src/common/utils.py
+++ src/common/utils.py
@@ -2,11 +2,16 @@ from datetime import datetime
 import sys; sys.path.append('..')
 
 
-# -----------------------------------------------------------------
-# functions to scale and re-scale parameters
-# -----------------------------------------------------------------
-# scale all parameters to [0,1]
 def utils_scale_parameters(limits, parameters):
+    """
+    This function scales a set of parameters of a given range (defined by limits) to the range [0,1]
+
+    Args:
+        limits: A 2D list or array defining the limits of the parameter object
+        parameters: A 2D list or array of parameters in the limits given by 'limits'
+    Returns:
+        The parameter 2D object scaled to [0,1]
+    """
 
     for i in range(parameters.shape[1]):
 
@@ -18,8 +23,16 @@ def utils_scale_parameters(limits, parameters):
     return parameters
 
 
-# re-scale all parameters to original limits
 def utils_rescale_parameters(limits, parameters):
+    """
+    This function re-scales a set of parameters in the range [0,1] to a given range defined by limits
+
+    Args:
+        limits: A 2D list or array of limits to re-scale to
+        parameters: A 2D list or array of parameters in the range [0,1]
+    Returns:
+        The re-scaled 1D parameter object
+    """
 
     for i in range(parameters.shape[1]):
 
@@ -31,8 +44,16 @@ def utils_rescale_parameters(limits, parameters):
     return parameters
 
 
-# re-scale a single given parameter
 def utils_rescale_parameters_single(limits, p):
+    """
+    This function re-scales a given parameter vector in the range [0,1] to a range defined by 'limits'
+
+    Args:
+        limits: A 2D list or array of limits to re-scale to
+        p: A parameter vector gives as 1D list or array
+    Returns:
+        The re-scaled 1D parameter object
+    """
 
     for i in range(0, len(p)):
 
@@ -44,9 +65,11 @@ def utils_rescale_parameters_single(limits, p):
     return p
 
 
-# -----------------------------------------------------------------
-# Current time stamp as a string
-# -----------------------------------------------------------------
 def utils_get_current_timestamp():
+    """
+    Get a current time stamp
 
+    Returns:
+        Custom formatted date time string
+    """
     return datetime.now().strftime('%Y_%m_%d__%H_%M_%S')
