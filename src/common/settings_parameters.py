7a41cd402792d46942137ef9cae69cbb10ac5463
Aswin Vijayan <aswvij@g-12-l0002.cm.cluster>
2022-04-21 16:03:59 +0200

    changed latex style so not to break

--- src/common/settings_parameters.py
+++ src/common/settings_parameters.py
@@ -1,22 +1,24 @@
 # Settings related to the parameters and their ranges can go in here
 
 # -----------------------------------------------------------------
-#  Settings for data sets with 5 parameters
+#  Settings for data sets with 7 parameters
 # -----------------------------------------------------------------
 
-PARAMETER_NUMBER = 6
+PARAMETER_NUMBER = 7
 
-# Parameter names           Intervals                   Units
-# 1. Gas density            interval=[-3.0, 6.0]        log (cm^-3)
-# 2. Gas phase metallicity  interval=[-3.0, 0.30103]    Solar metallicity
-# 3. Redshift               interval=[3.0, 12.0]        Absolute value
-# 4. ionization parameter   interval=[-4.0, 0.0]        See Hazy 5.8
-# 5. Stellar metallicity    interval=[-5, -1.3979]      Absolute value
-# 6. Stellar age            interval=[1.0, 2000.0]      Myr
+# Parameter names           Sampling intervals          Sampling units          Cloudy units
+# 1. Gas density            interval=[-3.0, 6.0]        log_10 (cm^-3)          same
+# 2. Gas phase metallicity  interval=[-3.0, 0.30103]    log_10( Z_solar)        10^()
+# 3. Redshift               interval=[3.0, 12.0]        Absolute value          same
+# 4. CR ionization factor   interval=[1.0, 3.0]         See Hazy X.y            10^()
+# 5. ionization parameter   interval=[-4.0, 0.0]        See Hazy 5.8            same
+# 6. Stellar metallicity    interval=[-5, -1.3979]      Absolute value          10^()
+# 7. Stellar age            interval=[1.0, 2000.0]      Myr                     ()*1e6
 
 PARAMETER_LIMITS = [[-3.0, 6.0],
                     [-3.0, 0.30103],
                     [3.0, 12.0],
+                    [1.0, 3.0],
                     [-4.0, 0.0],
                     [-5.0, -1.3979],
                     [1.0, 2000.0]
@@ -24,16 +26,16 @@ PARAMETER_LIMITS = [[-3.0, 6.0],
 
 PARAMETER_NUMBER_GAS_PHASE_METALLICITY = 2
 PARAMETER_NUMBER_REDSHIFT = 3
-PARAMETER_NUMBER_STELLAR_METALLICITY = 5
-PARAMETER_NUMBER_STELLAR_AGE = 6
+PARAMETER_NUMBER_CR_SCALING = 4
+PARAMETER_NUMBER_STELLAR_METALLICITY = 6
+PARAMETER_NUMBER_STELLAR_AGE = 7
 
 # LaTeX names of our parameters (for matplotlib)
-PARAMETER_NAMES_LATEX =['\log_{10}(\\rho_{\mathrm{gas}} \, \mathrm{cm^{3}})',
-                        '\log_{10}(Z_{\mathrm{gas}}/Z_{\\odot})',
+PARAMETER_NAMES_LATEX =['\mathrm{log}_{10}(\rho_{\mathrm{gas}} / \mathrm{cm^{3}})',
+                        '\mathrm{log}_{10}(Z_{\mathrm{gas}} / Z_{\odot})',
                         'z',
+                        '\mathrm{CR\, scaling}',
                         '\mathrm{U}',
-                        '\log_{10}(Z_{\\ast})',
-                        't_{\\ast} \; [Myr]'
+                        '\mathrm{log}_{10}(Z_{\star})',
+                        't_{\star} / \mathrm{Myr}'
                         ]
-
-
